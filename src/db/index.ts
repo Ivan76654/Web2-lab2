3b45a1adf6270e3d4c036a29111b42c4a318b0ef
Ivan76654 <ivan.lovric2101@gmail.com>
2023-11-13 07:17:25 +0100

    Add render database configuration.

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,12 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
+const config = process.env.RENDER_DATABASE ? {
+  connectionString: process.env.RENDER_DATABASE
+} : {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
-});
+};
+
+const pool = new Pool(config);
 
 export default pool;
